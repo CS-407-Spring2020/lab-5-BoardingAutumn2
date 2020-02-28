89f1cf5e0b6da96471464fb2455c3d7e76f0ee9d
BoardingAutumn2 <46009416+BoardingAutumn2@users.noreply.github.com>
2020-02-27 18:12:54 -0600

    update 2

--- app/src/main/java/c/sakshi/lab5/MainActivity.java
+++ app/src/main/java/c/sakshi/lab5/MainActivity.java
@@ -1,9 +1,26 @@
 package c.sakshi.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction (View view){
+        EditText username = (EditText) findViewById(R.id.username);
+        String str = username.getText().toString();
+
+        goToActivity2(str);
+    }
+
+    public void goToActivity2 (String s) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("name", s);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
